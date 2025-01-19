c7066ada7458d87e3bac24815bd78f978b0b4dbc
Omar <omardavidjm@ufps.edu.co>
2025-01-19 16:52:43 -0500

    feat: update database configuration and improve connection pooling settings

--- src/config.js
+++ src/config.js
@@ -1,14 +1,9 @@
-import {config} from 'dotenv';
-
+import { config } from "dotenv";
 config();
 
-console.log(process.env.PORT)
-
 export const PORT = process.env.PORT || 3000;
-export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'my-secret-pw';
-export const DB_HOST = process.env.DB_HOST || '35.222.224.163';
-export const DB_DATABASE = process.env.DB_DATABASE || 'musicapp';
-export const DB_PORT = process.env.DB_PORT || 3306;
-export const SECRET = process.env.SECRET || 'musicapp-api';
-
+export const DB_HOST = process.env.DB_HOST || "34.136.80.217";
+export const DB_USER = process.env.DB_USER || "root";
+export const DB_PASSWORD = process.env.DB_PASSWORD || ";=W_9N+wbxm4K5(MFAo]";
+export const DB_DATABASE = process.env.DB_DATABASE || "farmacia";
+export const SECRET = process.env.SECRET ||"MUSICAPP";
