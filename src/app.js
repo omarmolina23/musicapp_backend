dac50e57a771e8f3cb3eb5f35df2146cc68aaeb3
Omar <omardavidjm@ufps.edu.co>
2025-01-20 15:33:06 -0500

    feat: enable CORS support for the application

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ import express from "express";
 import fileUpload from "express-fileupload";
 import songsRoutes from "./routes/songs.routes.js";
 import authRoutes from "./routes/auth.routes.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 app.use(fileUpload({
     useTempFiles: true,
