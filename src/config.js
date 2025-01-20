4aa2c9763cde1ae0042bd37650be0fe386dabda3
Omar <omardavidjm@ufps.edu.co>
2025-01-20 15:21:11 -0500

    feat: update database host configuration

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || "34.123.12.58";
+export const DB_HOST = process.env.DB_HOST || "34.45.46.36";
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || ";=W_9N+wbxm4K5(MFAo]";
 export const DB_DATABASE = process.env.DB_DATABASE || "farmacia";
