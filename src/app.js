91e11b874db43430ed9ff92ab00b8c29da22caea
Omar <omardavidjm@ufps.edu.co>
2025-01-27 15:25:46 -0500

    feat: implement dynamic CORS origin handling in app.js

--- src/app.js
+++ src/app.js
@@ -12,8 +12,14 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "*",
-    credentials: true
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    },
+    credentials: true 
 }));
 
 app.use(express.json());
